402d1adbce82883a9caa18729c0333e0a282dfbd
rioso <rioso@DESKTOP-NMQ7GV0>
2020-10-07 20:20:57 -0700

    second version

--- CSE120_Fall2020/src/cse360assignment02/AddingMachine.java
+++ CSE120_Fall2020/src/cse360assignment02/AddingMachine.java
@@ -2,29 +2,69 @@ package cse360assignment02;
 
 public class AddingMachine {
 
+
 	// private instance variable
 	private int total;
+	private String history;
 
-	//constructor, initializes the instance variable
+	// constructor, initializes the instance variable
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
+		history = "0";
+	}
+	
+	 public static void main(String[] args) {
+		 
+		 // TESTING
+		 
+		// this statement creates an AddingMachine class object
+		AddingMachine addMachine = new AddingMachine();
+
+		addMachine.add(4); // calls add()
+
+		addMachine.subtract(2); // calls subtract()
+
+		addMachine.add(5);
+
+		System.out.println(addMachine.toString());
+
+		System.out.println("Total = " + addMachine.getTotal());
+
+		addMachine.clear();
+
+		System.out.println(addMachine.toString());
+
+		System.out.println("Total = " + addMachine.getTotal());
+
+		// TESTING COMPLETE: SUCCESSFUL
 	}
 
-	public int getTotal() {
-		return 0;
+	public int getTotal() { // returns the value of total
+		
+		return total;
 	}
 
-	public void add(int value) {
+	public void add(int value) { // adds the parameter value to total & updates history
+		
+		total += value;
+		history += " + " + value;
 	}
 
-	public void subtract(int value) {
+	public void subtract(int value) { // subtracts the parameter value from total & updates history
+		
+		total -= value;
+		history += " - " + value;
 	}
 
-	public String toString() {
-		return "";
+	public String toString() { // returns the history String
+		
+		return history;
 	}
 
-	public void clear() {
+	public void clear() { //restarts all the variables to their initial value
+		
+		total = 0;
+		history = "0";
 	}
 
-} //End of AddingMachine.java file
+} // End of AddingMachine.java file
