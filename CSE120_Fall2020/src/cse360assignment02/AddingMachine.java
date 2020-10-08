2b10eb646077b2230b809cea86c2f1422380e1a3
rioso <rioso@DESKTOP-NMQ7GV0>
2020-10-07 19:26:45 -0700

    assignment 1, following instructions.

---
+++ CSE120_Fall2020/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assignment02;
+
+public class AddingMachine {
+
+	// private instance variable
+	private int total;
+
+	//constructor, initializes the instance variable
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	public int getTotal() {
+		return 0;
+	}
+
+	public void add(int value) {
+	}
+
+	public void subtract(int value) {
+	}
+
+	public String toString() {
+		return "";
+	}
+
+	public void clear() {
+	}
+
+} //End of AddingMachine.java file
